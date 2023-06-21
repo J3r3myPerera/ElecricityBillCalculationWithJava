3fda4014539d9b955b7554e6ae81d58fb3b95788
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-06-21 09:10:12 +0530

    Adding the columns

--- ElecricityBill/src/Project.java
+++ ElecricityBill/src/Project.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.security.Key;
 
 public class Project  extends JFrame implements ActionListener {
 
@@ -16,6 +19,85 @@ public class Project  extends JFrame implements ActionListener {
         JLabel l1 = new JLabel(icc3);
         add(l1);
 
+//        For the first column
+        JMenuBar mb = new JMenuBar();
+        JMenu master = new JMenu("Master");
+        JMenuItem m1 = new JMenuItem("New customer");
+        JMenuItem m2 =new JMenuItem("Customer details");
+        JMenuItem m3 = new JMenuItem("Deposit Details");
+        master.setForeground(Color.BLUE);
 
+//        Customer details
+        m1.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon1 = new ImageIcon(ClassLoader.getSystemResource("images/icon1.jpg"));
+        Image image1 = icon1.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        m1.setIcon(new ImageIcon(image1));
+        m1.setMnemonic('D');
+        m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+        m1.setBackground(Color.WHITE);
+
+//        meter details
+        m2.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon2 = new ImageIcon(ClassLoader.getSystemResource("images/icon2.png"));
+        Image image2 = icon2.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        m2.setIcon(new ImageIcon(image2));
+        m2.setMnemonic('M');
+        m2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+        m2.setBackground(Color.WHITE);
+
+//        Deposit details
+        m3.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon3 = new ImageIcon(ClassLoader.getSystemResource("images/icon3.png"));
+        Image image3 = icon3.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        m3.setIcon(new ImageIcon(image3));
+        m3.setMnemonic('N');
+        m3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        m3.setBackground(Color.WHITE);
+
+        m1.addActionListener(this);
+        m2.addActionListener(this);
+        m3.addActionListener(this);
+
+//        ------------------------------------------
+
+//        second column
+        JMenu user = new JMenu("User");
+        JMenuItem u1 = new JMenuItem("PayBill");
+        JMenuItem u2 = new JMenuItem("Calculate bill");
+        JMenuItem u3 = new JMenuItem("Last Bill");
+        user.setForeground(Color.RED);
+
+//        Pay bill
+        u1.setFont(new Font("monospaced", Font.PLAIN,12));
+        ImageIcon icon4 = new ImageIcon(ClassLoader.getSystemResource("images/icon4.png"));
+        Image image4= icon4.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        u1.setIcon(new ImageIcon(image4));
+        u1.setMnemonic('P');
+        u1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        u1.setBackground(Color.WHITE);
+
+//        Bill detials
+        u2.setFont(new Font("monospaced", Font.PLAIN,12));
+        ImageIcon icon5 = new ImageIcon(ClassLoader.getSystemResource("images/icon5.png"));
+        Image image5= icon5.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        u2.setIcon(new ImageIcon(image5));
+        u2.setMnemonic('B');
+        u2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+        u2.setBackground(Color.WHITE);
+
+//        Last bill
+        u3.setFont(new Font("monospaced", Font.PLAIN,12));
+        ImageIcon icon6 = new ImageIcon(ClassLoader.getSystemResource("images/icon6.png"));
+        Image image6= icon6.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        u3.setIcon(new ImageIcon(image6));
+        u3.setMnemonic('L');
+        u3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+        u3.setBackground(Color.WHITE);
+
+        u1.addActionListener(this);
+        u2.addActionListener(this);
+        u3.addActionListener(this);
+
+//        ---------------------------------------------------
     }
 }
