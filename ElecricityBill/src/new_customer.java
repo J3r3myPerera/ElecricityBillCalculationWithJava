69f501a8385202f29a733b31210ab92ced041698
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-06-29 11:55:11 +0530

    new_customer class code

--- ElecricityBill/src/new_customer.java
+++ ElecricityBill/src/new_customer.java
@@ -1,2 +1,19 @@
-public class new_customer {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class new_customer extends JFrame implements ActionListener {
+    JLabel l1,l2,l3,l4,l5,l6,l7,l8;
+    JTextField t1,t2,t3,t4,t5,t6,t7;
+    JButton b1,b2;
+    new_customer() {
+        super("Add customer");
+        setLocation(350,200);
+        setSize(650,600);
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(9,2,10,10));
+
+        p.setBackground(Color.WHITE);
+    }
 }
